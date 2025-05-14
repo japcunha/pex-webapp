d2723f17a03c13f92515a573c3d9a7795150da79
Joane <138986247+japcunha@users.noreply.github.com>
2025-05-14 17:56:01 -0300

    feat: carrossel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,61 @@
+import { useState, useEffect } from "react";
+
+const imagens = [
+  "café (1).jpg",
+  "café (2).jpg",
+  "café (3).jpg",
+  "café (4).jpg",
+  "café (5).jpg",
+  "café (6).jpg",
+];
+
 export default function Home() {
+  const [indiceAtual, setIndiceAtual] = useState(0);
+
+  useEffect(() => {
+    const intervalo = setInterval(() => {
+      setIndiceAtual((prev) => (prev + 1) % imagens.length);
+    }, 5000);
+
+    return () => clearInterval(intervalo); // limpa o intervalo 
+  }, []);
+
   return (
     <div className="px-4 py-15 ">
-      <h1 className="text-4xl text-center font-bold text-white pt-25">
+      <h1 className="text-5xl font-bold text-center text-white drop-shadow-l mt-20">
         Bem-vindo à Cafeteria{" "}
         <span className="text-amber-100">
           <p>Fino Grão</p>
         </span>
       </h1>
-      <div className="bg-black/80  p-10 m-18 mt-30">
-        <p className="text-white text-center mt-10">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident eos
-          excepturi deserunt nihil repudiandae obcaecati sint voluptates non
-          deleniti impedit aperiam esse ipsum, rem doloremque eum expedita
-          dolore illum animi!Lorem ipsum dolor sit amet consectetur adipisicing
-          elit.
-        </p>
+      {/*texto e carrossel:*/}
+         <div className="flex justify-center mt-20">
+        <div className="relative w-full max-w-4xl h-[400px] overflow-hidden rounded-lg shadow-lg">
+          
+          <div className="absolute inset-0 bg-black/40 flex items-center justify-center p-8 z-10">
+            <p className="text-white text-lg text-center leading-relaxed max-w-xl">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident eos
+              excepturi deserunt nihil repudiandae obcaecati sint voluptates non
+              deleniti impedit aperiam esse ipsum, rem doloremque eum expedita
+              dolore illum animi!
+            </p>
+          </div>
+
+          <div
+            className="flex transition-transform duration-700 ease-in-out h-full"
+            style={{ transform: `translateX(-${indiceAtual * 100}%)` }}
+          >
+            {imagens.map((src, index) => (
+              <img
+                key={index}
+                src={src}
+                alt={`Slide ${index + 1}`}
+                className="w-full flex-shrink-0 object-cover h-full"
+              />
+            ))}
+          
+          </div>
+        </div>
       </div>
     </div>
   );
